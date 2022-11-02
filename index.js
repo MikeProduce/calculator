4802c7d66ec5577c40f4f1d54283479e0ebf324f
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-02 07:09:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const buttons = document.getElementsByClassName("button");
-const inputs = document.getElementsByClassName("input");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", () => {
-        let inputchanges = inputs.value;
-        return (inputchanges = buttons[i].innerHTML);
-
+        console.log(buttons[i].innerHTML);
+    });
+}
