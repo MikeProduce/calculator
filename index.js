0f41b7fc5982b5a46d5d2aa7d9cb9b4819d254a8
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2022-11-02 07:22:54 -0600

    its not working what

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const buttons = document.getElementsByClassName("button");
+"strict";
+
+const buttons = document.querySelectorAll(".button");
 
 for (let i = 0; i < buttons.length; i++) {
+    console.log(buttons[i]);
     buttons[i].addEventListener("click", () => {
         console.log(buttons[i].innerHTML);
     });
